aba6e071c013dc29078ff6c8ae221301ff121dcc
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2023-09-03 08:42:27 -0300

    Initial Commit

---
+++ MusicApp_SwiftUI/MusicApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp_SwiftUIApp.swift
+//  MusicApp_SwiftUI
+//
+//  Created by Leonardo Bai on 03/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
